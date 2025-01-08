8a26bb11b44d66fdaaaa6c761336166652e8b73d
arnab-mkj <arnjee05@gmail.com>
2025-01-09 00:58:34 +0100

    Revert "simple llm"
    This reverts commit d1237b5a0549eece86ca824c6d0c325735babc80.

--- read_parquet.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Read a Parquet file into a DataFrame
-df = pd.read_parquet('E:/progs/llm/openwebtext_en/data/train-00000-of-00020-567acd907ed91a97.parquet')
-
-# Display the first few rows of the DataFrame
-print(df.head())
